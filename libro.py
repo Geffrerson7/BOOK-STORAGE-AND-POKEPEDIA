ab94c946acc306f09bba081bd3716092366b9ade
Gefferson Casasola <gcasasolah@uni.pe>
2022-10-31 19:30:33 -0500

    creacion de clase y constructor

---
+++ libro.py
@@ -0,0 +1,12 @@
+from typing_extensions import Self
+
+
+class Libro:
+    def __init__(self, id, titulo, genero, ISBN, editorial, autores):
+        self.__id = id
+        self.__titulo = titulo
+        self.__genero = genero
+        self.__ISBN = ISBN
+        self.__editorial = editorial
+        self.__autores = autores
+        
