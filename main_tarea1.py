4338f83b501dd8a73b5501c3fb2e720eb14ff316
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-11-01 13:42:25 -0500

    Funcion de creación del menú de opciones

--- main_tarea1.py
+++ main_tarea1.py
@@ -1,30 +1,37 @@
 from funciones_tarea1 import *
 from funciones_utilidades import *
-lista_libros=[]
+lista_libros = []
 
 while True:
-    opcion = validarLeerInt("Ingrese la opción:")
+    print("***** BIBLIOTECA *******")
+    creacionMenu(["Leer archivo de disco duro", "Listar libros", "Agregar libro",
+                 "Eliminar libro", "Buscar libro por ISBN o por título", "Ordenar libros por título",
+                  "Buscar libros por autor, editorial o género", "Buscar libros por número de autores",
+                  "Editar o actualizar datos de un libro", "Guardar libros en archivo","Salir"])
+    opcion = validarRangoInt(1,11,"Ingrese la opción:")
     if opcion == 1:
         lista_libros.extend(CargarArchivo())
         id = 0
     elif opcion == 3:
-        id=crearId(len(lista_libros))
+        id = crearId(len(lista_libros))
         print("Ingrese datos del libro:")
-        titulo=validarLeerStrings(" -Título:")
-        genero=validarLeerStrings(" -Género: ")
-        isbn=validarLeerStrings(" -ISBN: ")
-        editorial=validarLeerStrings(" -Editorial: ")
-        nroAutores=validarLeerInt(" -Nro de autores:")
-        autores=leerArrayStrings("  ->Autor ",nroAutores)
-        libro=agregarLibro(id,titulo,genero,isbn,editorial,autores)
+        titulo = validarLeerStrings(" -Título:")
+        genero = validarLeerStrings(" -Género: ")
+        isbn = validarLeerStrings(" -ISBN: ")
+        editorial = validarLeerStrings(" -Editorial: ")
+        nroAutores = validarLeerInt(" -Nro de autores:")
+        autores = leerArrayStrings("  ->Autor ", nroAutores)
+        libro = agregarLibro(id, titulo, genero, isbn, editorial, autores)
         lista_libros.append(libro)
         print("[*** Libro agregado ****]")
-    elif opcion==6:
+    elif opcion == 6:
         print("*** Libros Ordenados Por titulo ****")
-        titulos=ordenarLibrosPorTitulo(lista_libros)
-        for i,titulo in enumerate(titulos,1):
-            print(i,titulo)
-    if opcion == 11:
+        titulos = ordenarLibrosPorTitulo(lista_libros)
+        for i, titulo in enumerate(titulos, 1):
+            print(i, titulo)
+    elif opcion == 9:
+        pass
+    elif opcion == 11:
         break
 for item in lista_libros:
     print(item.get_titulo())
