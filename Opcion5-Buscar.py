2c002e00458eb1140941839cf4c4bad185dca5e0
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-11-01 13:22:54 -0500

    modificando la opcion 5 de busqueda

--- Opcion5-Buscar.py
+++ Opcion5-Buscar.py
@@ -1,7 +1,12 @@
 import re
-def Buscar_libro_por_ISBN_o_título(input_data: str):
-    x = re.search("^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$", input_data)
-    if x:
+def Buscar_libro_por_ISBN_o_título(input_user: str, data):
+    ISBN = re.search("^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$", input_user)
+
+    titulo_list = input_user.split(" ")
+        
+    titulo = str.find("")
+
+    if ISBN | :
         print("YES! We have a match!")
     else:
         print("No match")
