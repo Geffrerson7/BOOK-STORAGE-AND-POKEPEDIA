77fd54dadb6b9df8a621bdec6936b5a85b7b1678
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-11-01 15:39:53 -0500

    Creacion de la opcion 9, modificacion de libro

--- funciones_tarea1.py
+++ funciones_tarea1.py
@@ -2,7 +2,7 @@ import csv
 import os
 os.system("cls")
 from libro import *
-
+from funciones_utilidades import *
 
 def CargarArchivo() -> list:
         direccion = input("Escriba la dirección del archivo .txt o .csv: ")
@@ -21,15 +21,75 @@ def CargarArchivo() -> list:
 def crearId(longitudLibros:list[Libro])->int:
     return longitudLibros
 
-def agregarLibro(lastId: int, titulo: str, genero: str, isbn: str, editorial: str, autores: list[str]) -> Libro:
+def crearLibro(lista_libros:list[Libro]) -> Libro:
+    lastId = crearId(len(lista_libros))
+    print("Ingrese datos del libro:")
+    titulo = validarLeerStrings(" -Título:")
+    genero = validarLeerStrings(" -Género: ")
+    isbn = validarLeerStrings(" -ISBN: ")
+    editorial = validarLeerStrings(" -Editorial: ")
+    nroAutores = validarLeerInt(" -Nro de autores:")
+    autores = leerArrayStrings("  ->Autor ", nroAutores)
     libroCreado = Libro(lastId, titulo, genero, isbn, editorial, autores)
     return libroCreado
 
 def ordenarLibrosPorTitulo(libros:list[Libro])->list[str]:
-    titulosOrdenados=[]
-    for libro in libros:
-        titulosOrdenados.append(libro.get_titulo())
+    titulosOrdenados=[libro.get_titulo() for libro in libros]
     return sorted(titulosOrdenados)
 
-def actualizarLibro():
-    pass
+def actualizarLibro(libro:Libro)->Libro:
+    variablesActualizar=["titulo","título","género", "genero", "isbn", "editorial", "autores"]
+    atributo=validarLeerStrings("¿Qué desea modificar [título,género,ISBN,editorial o autores]?: ").lower()
+    while True:
+        if(atributo in variablesActualizar):
+            if(atributo==variablesActualizar[0] or atributo==variablesActualizar[1]):
+                titulo = validarLeerStrings(" -Nuevo Título:")
+                libro.set_titulo(titulo)
+            elif(atributo==variablesActualizar[2] or atributo==variablesActualizar[3]):
+                genero = validarLeerStrings(" -Nuevo Género: ")
+                libro.set_genero(genero)
+            elif(atributo==variablesActualizar[4]):
+                isbn = validarLeerStrings(" -Nuevo ISBN: ")
+                libro.set_ISBN(isbn)
+            elif(atributo==variablesActualizar[5]):
+                editorial = validarLeerStrings(" -Nueva Editorial: ")
+                libro.set_editorial(editorial)
+            elif(atributo==variablesActualizar[6]):
+                autores=modificarAutor(libro)
+                libro.set_autores(autores)
+            return libro
+        atributo=validarLeerStrings("¿Qué desea modificar [título,género,ISBN,editorial o autores]?: ").lower()
+        print("[El valor ingresado no es una opción disponible]")
+
+def modificarAutor(libro:Libro)->list[str]:
+    print("**** Autores ****")
+    autores=libro.get_autores()
+    creacionMenu(["Agregar autor","Eliminar autor","Modificar autor existente"])
+    op=validarRangoInt(1,3,"Ingrese una opción: ")
+    if(op==1):
+        autor=validarLeerStrings(" ->Nuevo autor:")
+        autores.append(autor)
+    elif(op==2):
+        if(len(libro.get_autores())==1):
+            print("[ERROR: El libro debe tener al menos 1 autor]")
+        else:
+            autor=validarLeerStrings(" ->Nombre de autor a eliminar:")
+            if(autor in autores):
+                index=autores.index(autor)
+                autores.pop(index)
+            else:
+                print("[ERROR: El autor ingresado no existe]")
+    elif(op==3):
+        autor=validarLeerStrings(" ->Nombre de autor a modificar:")
+        if(autor in autores):
+            index=autores.index(autor)
+            autores[index]=autor
+        else:
+            print("[ERROR: El autor ingresado no existe]")
+    return autores
+
+def buscarISBN(isbn:str,lista_libros:list[Libro])->Libro:
+    for libro in lista_libros:
+        if(libro.get_ISBN()==isbn):
+            return libro,lista_libros.index(libro)
+    return None,-1
--- main_tarea1.py
+++ main_tarea1.py
@@ -13,24 +13,31 @@ while True:
         lista_libros.extend(CargarArchivo())
         id = 0
     elif opcion == 3:
-        id = crearId(len(lista_libros))
-        print("Ingrese datos del libro:")
-        titulo = validarLeerStrings(" -Título:")
-        genero = validarLeerStrings(" -Género: ")
-        isbn = validarLeerStrings(" -ISBN: ")
-        editorial = validarLeerStrings(" -Editorial: ")
-        nroAutores = validarLeerInt(" -Nro de autores:")
-        autores = leerArrayStrings("  ->Autor ", nroAutores)
-        libro = agregarLibro(id, titulo, genero, isbn, editorial, autores)
+        print("******* AGREGRAR LIBRO ********")
+        libro = crearLibro(lista_libros)
         lista_libros.append(libro)
         print("[*** Libro agregado ****]")
     elif opcion == 6:
-        print("*** Libros Ordenados Por titulo ****")
+        print("*** LIBROS ORDENADOS POR TÍTULO ****")
         titulos = ordenarLibrosPorTitulo(lista_libros)
         for i, titulo in enumerate(titulos, 1):
             print(i, titulo)
     elif opcion == 9:
-        pass
+        print("****** EDITAR LIBRO ******")
+        isbn=validarLeerStrings(" -Ingrese ISBN del libro a editar: ")
+        libroActualizar,index=buscarISBN(isbn,lista_libros)
+        if(libroActualizar is not None):
+            print("***** OPCIONES DE EDICIÓN ***** ")
+            creacionMenu(["Editar todos los datos del libro","Actualizar un dato en especifico","Cancelar"])
+            op=validarRangoInt(1,3,"Ingrese una opción: ")
+            if(op==1):
+                lista_libros[index]=crearLibro(lista_libros)
+            if(op==2):
+                libro=actualizarLibro(libroActualizar)
+                lista_libros[index]=libro
+        else:
+            print("[El ISBN ingresado no está registrado en el sistema]")
+
     elif opcion == 11:
         break
 for item in lista_libros:
