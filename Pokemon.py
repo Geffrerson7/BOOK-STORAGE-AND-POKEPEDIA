75fcac5bdea551752c99b3e6596c483c0c7f639f
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-11-04 20:46:13 -0500

    Creacion de funcion listar habilidades

--- Pokemon.py
+++ Pokemon.py
@@ -1,5 +1,5 @@
 class Pokemon:
-    def __init__(self,name,habilidades,urlImg) -> None:
-        self.name=name
-        self.habilidades=habilidades
-        self.urlImg=urlImg
+    def __init__(self, name, habilidades, urlImg) -> None:
+        self.name = name
+        self.habilidades = habilidades
+        self.urlImg = urlImg
